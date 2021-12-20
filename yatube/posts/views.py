786fba1ac9ffa0458de57e04e980305dc78a364f
Алексей Шебеко <alexprotos001@gmail.com>
2021-12-20 09:38:33 +0300

    настройка html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,27 @@
+from typing import Text
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = 'последния обновления сайта'
+    text = "это главнаня страница проэкта Yatube"
+    context ={
+        "text" : text,
+        'title': title,
+    }
+    return render(request, template,context)
 
 def group_posts(request , slug):
-    return HttpResponse('Посты групы')
+    template = 'posts/group_list.html'
+    title = 'посты выбраной групы'
+    text = "здесь будут информация о групах пректа Yatube"
+    context = {
+        'text': text,
+        'title': title,
+        }
+    return render(request, template,context)
+
+
+
 
