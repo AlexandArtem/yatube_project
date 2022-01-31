eb3a05733fd2e80533be9e8f86a2638ef5a1c0b1
Алексей Шебеко <alexprotos001@gmail.com>
2022-01-31 09:48:45 +0300

    shit commit

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,9 +1,8 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
 
 urlpatterns = [
-    path('', include('posts.urls')),
+    path('', include('posts.urls', namespace = 'posts')),
     path('admin/', admin.site.urls),
     
 ]
